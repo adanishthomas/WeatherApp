7ef854d9039c84630a52807af617f7b993250f1a
Danish Thomas <a.danishthomas@gmail.com>
2022-10-12 23:21:43 +0530

    deploy

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,9 @@
+
 export const environment = {
-  production: true
+  production: true,
+  weatherApiBaseUrl: 'https://weatherapi-com.p.rapidapi.com/current.json',
+  XRapidAPIHostHeaderName: 'X-RapidAPI-Host',
+  XRapidAPIHostHeaderValue: 'weatherapi-com.p.rapidapi.com',
+  XRapidAPIKeyHeaderName: 'X-RapidAPI-Key',
+  XRapidAPIKeyHeaderValue: '5c1d53b110mshf60d8b0fe4a978dp103ce5jsn02bbdb4538ac'
 };
