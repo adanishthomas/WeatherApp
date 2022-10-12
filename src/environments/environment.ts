13d0cfb66ff8495a728d58d83319660f4871116c
Danish Thomas <a.danishthomas@gmail.com>
2022-10-12 18:30:02 +0530

    Completed

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,12 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  weatherApiBaseUrl: 'https://weatherapi-com.p.rapidapi.com/current.json',
+  XRapidAPIHostHeaderName: 'X-RapidAPI-Host',
+  XRapidAPIHostHeaderValue: 'weatherapi-com.p.rapidapi.com',
+  XRapidAPIKeyHeaderName: 'X-RapidAPI-Key',
+  XRapidAPIKeyHeaderValue: '5c1d53b110mshf60d8b0fe4a978dp103ce5jsn02bbdb4538ac'
 };
 
 /*
